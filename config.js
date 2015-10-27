03734a729fdbe47f2373301667456954eb042be8
Matthew Bauer <mjbauer95@gmail.com>
2015-10-27 01:29:45 -0500

    Update builds

--- config.js
+++ config.js
@@ -16,22 +16,29 @@ System.config({
       "index.coffee!github:forresto/system-coffee@0.1.2",
       "github:satazor/sparkmd5@1.0.1",
       "github:stuk/jszip@2.5.0",
+      "npm:localforage@1.3.0",
       "github:satazor/sparkmd5@1.0.1/spark-md5",
+      "npm:localforage@1.3.0/dist/localforage",
       "github:stuk/jszip@2.5.0/dist/jszip",
+      "github:jspm/nodelibs-process@0.1.2",
+      "settings.json!github:systemjs/plugin-json@0.1.0",
+      "github:jspm/nodelibs-process@0.1.2/index",
       "github:matthewbauer/x-game@1.1.2",
+      "npm:process@0.11.2",
       "github:matthewbauer/x-game@1.1.2/x-retro",
-      "github:matthewbauer/window@0.0.3",
       "github:matthewbauer/document@0.0.4",
+      "npm:process@0.11.2/browser",
       "npm:babel-runtime@5.8.25/core-js/object/create",
+      "github:matthewbauer/window@0.0.3",
       "github:matthewbauer/x-game@1.1.2/player.coffee!github:forresto/system-coffee@0.1.2",
-      "github:matthewbauer/window@0.0.3/window",
       "github:matthewbauer/document@0.0.4/document",
       "npm:core-js@1.2.1/library/fn/object/create",
-      "github:mohayonao/web-audio-api-shim@0.3.0",
+      "github:matthewbauer/window@0.0.3/window",
       "github:webcomponents/webcomponentsjs@0.7.14",
       "npm:core-js@1.2.1/library/modules/$",
-      "github:mohayonao/web-audio-api-shim@0.3.0/build/web-audio-api-shim",
-      "github:webcomponents/webcomponentsjs@0.7.14/webcomponents-lite"
+      "github:mohayonao/web-audio-api-shim@0.3.0",
+      "github:webcomponents/webcomponentsjs@0.7.14/webcomponents-lite",
+      "github:mohayonao/web-audio-api-shim@0.3.0/build/web-audio-api-shim"
     ]
   },
 
@@ -43,7 +50,9 @@ System.config({
     "document": "github:matthewbauer/document@0.0.4",
     "gambatte": "npm:gambatte@0.4.6",
     "gw": "npm:gw@0.4.6",
+    "json": "github:systemjs/plugin-json@0.1.0",
     "jszip": "github:stuk/jszip@2.5.0",
+    "localforage": "npm:localforage@1.3.0",
     "nestopia": "npm:nestopia@0.4.6",
     "raw": "github:matthewbauer/plugin-raw@0.3.1",
     "snes9x-next": "npm:snes9x-next@0.4.6",
@@ -56,6 +65,9 @@ System.config({
     "x-game": "github:matthewbauer/x-game@1.1.2",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.2"
@@ -81,6 +93,9 @@ System.config({
       "sparkmd5": "github:satazor/sparkmd5@1.0.1",
       "window": "github:matthewbauer/window@0.0.3"
     },
+    "npm:asap@1.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
@@ -95,8 +110,19 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:localforage@1.3.0": {
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "promise": "npm:promise@5.0.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:promise@5.0.0": {
+      "asap": "npm:asap@1.0.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
